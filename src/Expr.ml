436989b5246ce8ee66702d860e230d7607b20788
Ekaterina Podranyuk <e.podranyuk@yandex.ru>
2019-02-21 03:29:26 +0300

    Added code for expression evaluator

--- src/Expr.ml
+++ src/Expr.ml
@@ -35,13 +35,38 @@ let update x v s = fun y -> if x = y then v else s y
 (* An example of a non-trivial state: *)                                                   
 let s = update "x" 1 @@ update "y" 2 @@ update "z" 3 @@ update "t" 4 empty
 
-(* Some testing; comment this definition out when submitting the solution. *)
+(* Some testing; comment this definition out when submitting the solution. 
 let _ =
   List.iter
     (fun x ->
        try  Printf.printf "%s=%d\n" x @@ s x
        with Failure s -> Printf.printf "%s\n" s
     ) ["x"; "a"; "y"; "z"; "t"; "b"]
+*)
+
+(* Some helping code for further work:
+    boolToInt converts boolean b to integer
+    intToBool converts integer i to boolean
+*)  
+let boolToInt b = if b then 1 else 0
+let intToBool i = i != 0
+
+
+(* Possible operations *)
+let operation oper leftExpr rightExpr = match oper with
+    |"!!" -> boolToInt (( || ) (intToBool leftExpr) (intToBool rightExpr))
+    |"&&" -> boolToInt (( && ) (intToBool leftExpr) (intToBool rightExpr))
+    |"==" -> boolToInt (( == ) leftExpr rightExpr)
+    |"!=" -> boolToInt (( != ) leftExpr rightExpr)
+    |"<=" -> boolToInt (( <= ) leftExpr rightExpr)
+    |"<" -> boolToInt (( <  ) leftExpr rightExpr)
+    |">=" -> boolToInt (( >= ) leftExpr rightExpr)
+    |">" -> boolToInt (( >  ) leftExpr rightExpr)
+    |"+" -> ( +  ) leftExpr rightExpr
+    |"-" -> ( -  ) leftExpr rightExpr
+    |"*" -> ( *  ) leftExpr rightExpr
+    |"/" -> ( /  ) leftExpr rightExpr
+    |"%" -> ( mod ) leftExpr rightExpr
 
 (* Expression evaluator
 
@@ -50,5 +75,8 @@ let _ =
    Takes a state and an expression, and returns the value of the expression in 
    the given state.
 *)
-let eval = failwith "Not implemented yet"
-                    
+let rec eval state expr = match expr with
+    |Const cName -> cName
+    |Var varName -> state varName
+    |Binop (oper, leftExpr, rightExpr) -> 
+        operation oper (eval state leftExpr) (eval state rightExpr);;
