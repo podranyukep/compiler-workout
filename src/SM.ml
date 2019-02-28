ebe3933561389c00618784d21cfbaeea66b36591
Ekaterina Podranyuk <e.podranyuk@yandex.ru>
2019-02-28 15:55:58 +0300

    Added code for second hw

--- src/SM.ml
+++ src/SM.ml
@@ -1,5 +1,7 @@
 open GT       
-       
+
+open List
+
 (* The type for the stack machine instructions *)
 @type insn =
 (* binary operator                 *) | BINOP of string
@@ -23,7 +25,17 @@ type config = int list * Syntax.Stmt.config
 
    Takes a configuration and a program, and returns a configuration as a result
  *)                         
-let eval _ = failwith "Not yet implemented"
+ let rec eval cfg prg =
+  let step (st, (s, i, o)) p = match p with
+    | BINOP op -> (Syntax.Expr.operation op (hd (tl st)) (hd st) :: (tl (tl st)), (s, i, o))
+    | CONST n  -> (n :: st, (s, i, o))
+    | READ     -> (hd i :: st, (s, tl i, o))
+    | WRITE    -> (tl st, (s, i, o @ [hd st]))
+    | LD variable_name    -> (s variable_name :: st, (s, i, o))
+    | ST variable_name    -> (tl st, (Syntax.Expr.update variable_name (hd st) s, i, o))
+  in match prg with
+    | [] -> cfg
+    | p :: ps -> eval (step cfg p) ps
 
 (* Top-level evaluation
 
@@ -41,4 +53,13 @@ let run i p = let (_, (_, _, o)) = eval ([], (Syntax.Expr.empty, i, [])) p in o
    stack machine
  *)
 
-let compile _ = failwith "Not yet implemented"
+let rec compile_expr e = match e with
+    | Syntax.Expr.Const n -> [CONST n]
+    | Syntax.Expr.Var v -> [LD v]
+    | Syntax.Expr.Binop (op, l_e,r_e) -> compile_expr l_e@ compile_expr r_e@ [BINOP op]
+
+let rec compile p = match p with
+    | Syntax.Stmt.Read variable_name -> [READ; ST variable_name]
+    | Syntax.Stmt.Write expression  -> compile_expr expression @ [WRITE]
+    | Syntax.Stmt.Assign (variable_name, expression) -> compile_expr expression@ [ST variable_name]
+    | Syntax.Stmt.Seq (e1, e2) -> compile e1 @ compile e2;;
--- src/Syntax.ml
+++ src/Syntax.ml
@@ -2,7 +2,9 @@
    The library provides "@type ..." syntax extension and plugins like show, etc.
 *)
 open GT 
-    
+
+open List
+
 (* Simple expressions: syntax and semantics *)
 module Expr =
   struct
@@ -34,6 +36,32 @@ module Expr =
     *)
     let update x v s = fun y -> if x = y then v else s y
 
+    
+    (* Some helping code for further work:
+    boolToInt converts boolean b to integer
+    intToBool converts integer i to boolean
+    *)  
+    let boolToInt b = if b then 1 else 0
+    let intToBool i = i != 0
+
+
+    (* Possible operations *)
+    let operation oper leftExpr rightExpr = match oper with
+        |"!!" -> boolToInt (( || ) (intToBool leftExpr) (intToBool rightExpr))
+        |"&&" -> boolToInt (( && ) (intToBool leftExpr) (intToBool rightExpr))
+        |"==" -> boolToInt (( == ) leftExpr rightExpr)
+        |"!=" -> boolToInt (( != ) leftExpr rightExpr)
+        |"<=" -> boolToInt (( <= ) leftExpr rightExpr)
+        |"<" -> boolToInt (( <  ) leftExpr rightExpr)
+        |">=" -> boolToInt (( >= ) leftExpr rightExpr)
+        |">" -> boolToInt (( >  ) leftExpr rightExpr)
+        |"+" -> ( +  ) leftExpr rightExpr
+        |"-" -> ( -  ) leftExpr rightExpr
+        |"*" -> ( *  ) leftExpr rightExpr
+        |"/" -> ( /  ) leftExpr rightExpr
+        |"%" -> ( mod ) leftExpr rightExpr
+    
+    
     (* Expression evaluator
 
           val eval : state -> t -> int
@@ -41,7 +69,11 @@ module Expr =
        Takes a state and an expression, and returns the value of the expression in 
        the given state.
     *)
-    let eval _ = failwith "Not implemented yet"
+    let rec eval state expr = match expr with
+    |Const cName -> cName
+    |Var varName -> state varName
+    |Binop (oper, leftExpr, rightExpr) -> 
+        operation oper (eval state leftExpr) (eval state rightExpr)
 
   end
                     
@@ -65,7 +97,11 @@ module Stmt =
 
        Takes a configuration and a statement, and returns another configuration
     *)
-    let eval _ = failwith "Not implemented yet"
+    let rec eval (s, i, o) p = match p with
+    | Read variable_name  -> (Expr.update variable_name  (hd i) s, tl i, o)
+    | Write expression   -> (s, i, o @ [Expr.eval s expression])
+    | Assign (variable_name, expression  ) -> (Expr.update variable_name (Expr.eval s expression ) s, i, o)
+    | Seq (e1, e2)  -> eval (eval (s, i, o) e1) e2   
                                                          
   end
 
