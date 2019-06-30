7cd531f99d4a9b6f3647805612356f3dcd89d51a
Ekaterina Podranyuk <e.podranyuk@yandex.ru>
2019-06-30 12:31:34 +0300

    Reorganized loop in SM

--- src/SM.ml
+++ src/SM.ml
@@ -124,11 +124,12 @@ let rec compileWithLabels p lastL =
                 ([LABEL lLoop] @ repeatBody @ expr e @ [CJMP ("z", lLoop)]), false
         | Stmt.ForEach (x, e1, e2, body) ->
             let lLoop = labelGen#get in
-            (*let lIncr = labelGen#get in*)
+            let lIncr = labelGen#get in
             let lQuit = labelGen#get in
-            let (doBody, _) = compileWithLabels body lQuit(*lIncr*) in
-                ((expr e1 @ [ST x]) @ [LABEL lLoop] @ [LD x] @ expr e1 @ [BINOP ">="] @ [CJMP("z", lQuit)] @ [LD x] @ expr e2 @ [BINOP "<="] @ [CJMP("z", lQuit)]
-                @ doBody (*@ [LABEL lIncr]*) @ [CONST 1] @ [LD x] @ [BINOP "+"] @ [ST x] @ [JMP lLoop] @ [LABEL lQuit]), false
+			let lCheck = labelGen#get in
+            let (doBody, _) = compileWithLabels body lIncr in
+                ((expr e1 @ [ST x]) @ [JMP lCheck; LABEL lLoop] @ doBody @ [LABEL lIncr] @ [CONST 1; LD x; BINOP "+"; ST x] @
+				[LABEL lCheck] @ [LD x] @ expr e1 @ [BINOP ">="; CJMP("z", lQuit); LD x] @ expr e2 @ [BINOP "<="; CJMP("nz", lLoop)] @ [LABEL lQuit]), false
         | Stmt.Skip -> [], false
 
 let rec compile p =
